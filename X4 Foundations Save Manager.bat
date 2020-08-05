1fa6fdb20600c0abdb34e1bcc210e8b17385fd35
ian.farr <ian.farr@farrworks.com>
2020-08-04 22:25:43 -0400

    controlling console height and width of cmd prompt

--- X4 Foundations Save Manager.bat
+++ X4 Foundations Save Manager.bat
@@ -2,4 +2,6 @@
 
 REM this is a simple wrapper batch file which will call backup_saves.ps1 in the current directory
 
+mode con: cols=120 lines=40
+
 powershell -NoLogo -ExecutionPolicy Unrestricted -File "%CD%\backup_saves.ps1"
